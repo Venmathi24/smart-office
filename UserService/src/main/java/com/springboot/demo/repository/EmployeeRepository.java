d4d6408353269c15c2c965830195b8692a589f2f
Venmathi24 <104155095+Venmathi24@users.noreply.github.com>
2025-06-02 14:47:37 +0530

    Add files via upload

---
+++ UserService/src/main/java/com/springboot/demo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.repository;
+
+import com.springboot.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmployeeCode(Long employeeCode);
+    Optional<Employee> findByName(String username);
+}
